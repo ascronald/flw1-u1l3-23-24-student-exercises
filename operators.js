ce89f338b9765d4a9b321ea255be7b9eeb4d1ffd
roszeto <27614699-roszeto@users.noreply.replit.com>
2023-10-25 22:25:05 +0000

    Assingment
    Finished student exercise for today

--- operators.js
+++ operators.js
@@ -5,17 +5,23 @@
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
-
+let num1=5;
+let num2=6;
+let sum=num1+num2;
+console.log(sum)
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
-
+let difference=num1-num2;
+console.log(difference)
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
+let product=num1*num2;
+console.log(product)
 
 
 
